1508a1a829803e12335b43e883d705779c607af8
wassim <lahianiwassim92@gmail.com>
2023-09-24 05:34:45 +0200

    ex4.js

---
+++ tdEcmascript/ex4.js
@@ -0,0 +1,4 @@
+const nom = "Alice";
+const age = 25;
+const message = `Bonjour, je m'appelle ${nom} et j'ai ${age} ans.`;
+console.log(message); // Attendu : "Bonjour, je m'appelle Alice et j'ai 25 ans."
